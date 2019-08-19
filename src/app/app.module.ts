4edf1cbc192d6315a071af714f80a700cffff007
iflyimpilot <iflyimpilot@gmail.com>
2019-08-19 14:12:57 -0400

    angular project first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { BucketlistPageComponent } from './bucketlist-page/bucketlist-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent,
+    EventListComponent,
+    BucketlistPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bucketlist-page/bucketlist-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bucketlist-page',
+  templateUrl: './bucketlist-page.component.html',
+  styleUrls: ['./bucketlist-page.component.css']
+})
+export class BucketlistPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
