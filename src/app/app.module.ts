691e250e09bd28e59b46f2b1e049dd8911ea650c
Hillary <uptonh13@gmail.com>
2019-08-19 14:45:22 -0400

    Import needed modules and create routing paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SearchCriteriaComponent } from "./search-criteria/search-criteria.component";
+import { EventListComponent } from "./event-list/event-list.component";
+import { BucketlistPageComponent } from "./bucketlist-page/bucketlist-page.component";
+import { FormsModule } from "@angular/forms";
+import { RoutingService } from "./routing.service";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { BucketlistPageComponent } from './bucketlist-page/bucketlist-page.component';
+const appRoutes: Routes = [
+  { path: "bucket-list", component: BucketlistPageComponent },
+  { path: "event-list", component: EventListComponent },
+  { path: "search-criteria", component: SearchCriteriaComponent },
+  { path: "", redirectTo: "/event-list", pathMatch: "full" }
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +25,11 @@ import { BucketlistPageComponent } from './bucketlist-page/bucketlist-page.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
-  providers: [],
+  providers: [RoutingService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
