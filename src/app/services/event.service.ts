9e7d87315a3e0436ee3fdbe855b04ad85275d409
Amber Metz <ambermetz88@gmail.com>
2019-08-19 16:24:03 -0400

    Added API & search

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class EventService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // Http client deals with observable so you need to say what type you are returning.
+  getData(keyword: string): Observable<any> {
+    return this.http
+      .get(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${keyword}&source=universe&countryCode=US&apikey=qeFcLJvWSqBRCQ0nFcMSyQWsI8rOcEGO
+    `);
+  }
 }
