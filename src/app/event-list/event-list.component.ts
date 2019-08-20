e9675d38e44411e6364b858399325bf2398fe9e8
Amber Metz <ambermetz88@gmail.com>
2019-08-20 11:07:00 -0400

    Events showing after search

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { EventService } from "../services/event.service";
 
 @Component({
-  selector: 'app-event-list',
-  templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.css']
+  selector: "app-event-list",
+  templateUrl: "./event-list.component.html",
+  styleUrls: ["./event-list.component.css"]
 })
 export class EventListComponent implements OnInit {
+  eventData: any[];
 
-  constructor() { }
+  constructor(private eventService: EventService) {}
 
   ngOnInit() {
+    this.eventData = this.eventService.eventList;
   }
-
 }
+
+// private searchCriteria: SearchCriteriaComponent
